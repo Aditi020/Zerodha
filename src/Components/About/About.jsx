df9b4c9e2322f919dd7bc4dc976777379ec9aa15
Aditi020 <aditi.kumar.2.2.24@gmail.com>
2025-01-11 02:07:07 +0530

    COCoerted Html css based Zerodha to Vite react based Zerodha

---
+++ src/Components/About/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Team from './Team'
+import Hero from './Hero'
+function About() {
+    return (
+        <div>
+            <Hero />
+            <Team />
+        </div>
+
+    )
+}
+
+export default About
